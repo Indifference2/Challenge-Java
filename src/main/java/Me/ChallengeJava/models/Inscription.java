214b87dfd6282d011a0e13a2f6e6b5aa45f0a878
Ivan San Miguel <ezequielivan2@gmail.com>
2023-07-05 03:55:03 -0300

    Make relationship ManyToOne with Course and Person

--- src/main/java/Me/ChallengeJava/models/Inscription.java
+++ src/main/java/Me/ChallengeJava/models/Inscription.java
@@ -1,9 +1,8 @@
 package Me.ChallengeJava.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -12,4 +11,20 @@ public class Inscription {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     private Long id;
     private LocalDateTime dateTime;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Person person;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Course course;
+    public Inscription() {
+    }
+    /* GETTERS */
+    public Long getId() {return id;}
+    public LocalDateTime getDateTime() {return dateTime;}
+    public Person getPerson() {return person;}
+    public Course getCourse() {return course;}
+
+    /* SETTERS */
+    public void setDateTime(LocalDateTime dateTime) {this.dateTime = dateTime;}
+    public void setPerson(Person person) {this.person = person;}
+    public void setCourse(Course course) {this.course = course;}
 }
