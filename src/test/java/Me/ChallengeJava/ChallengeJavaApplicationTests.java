438da83aa49807e4ee4a033912ae5e147d7206d4
Ivan San Miguel <ezequielivan2@gmail.com>
2023-07-04 22:51:07 -0300

    first commit

---
+++ src/test/java/Me/ChallengeJava/ChallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Me.ChallengeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
