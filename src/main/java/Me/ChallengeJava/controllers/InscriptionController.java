e3b44e482f5046cc2a74ea7228cb48d3dff94019
Ivan San Miguel <ezequielivan2@gmail.com>
2023-07-08 21:33:33 -0300

    Servlet modify and delete incription

--- src/main/java/Me/ChallengeJava/controllers/InscriptionController.java
+++ src/main/java/Me/ChallengeJava/controllers/InscriptionController.java
@@ -9,6 +9,7 @@ import Me.ChallengeJava.repositories.CourseRepository;
 import Me.ChallengeJava.repositories.InscriptionRepository;
 import Me.ChallengeJava.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -74,9 +75,45 @@ public class InscriptionController {
 
         return new ResponseEntity<>("Inscription successfully", HttpStatus.CREATED);
     }
-
+    // Modify an inscription
     @PutMapping("/api/inscriptions/{id}")
-    public ResponseEntity<Object> modifyInscription(@PathVariable Long id, @RequestBody Inscription inscription){
+    public ResponseEntity<Object> modifyInscription(@PathVariable Long id, @RequestBody InscriptionApplicationDTO inscriptionApplicationDTO){
+        // Get inscription
+        Inscription inscriptionToModify = inscriptionRepository.findById(id).orElse(null);
+        // Get User
+        User userToEnroll = userRepository.findById(inscriptionApplicationDTO.getUser_id()).orElse(null);
+        // Get Course
+        Course courseToEnroll = courseRepository.findById(inscriptionApplicationDTO.getCourse_id()).orElse(null);
+        // Validation inscription
+        if(inscriptionToModify == null){
+            return new ResponseEntity<>("Inscription doesn't exist", HttpStatus.FORBIDDEN);
+        }
+        // Validation User
+        if(userToEnroll == null){
+            return new ResponseEntity<>("User doesn't exist", HttpStatus.FORBIDDEN);
+        }
+        // Validation course
+        if(courseToEnroll == null){
+            return new ResponseEntity<>("Course doesn't exist", HttpStatus.FORBIDDEN);
+        }
+        // Validation shift
+        if(!inscriptionApplicationDTO.getShift().name().equalsIgnoreCase("MORNING") &&
+                !inscriptionApplicationDTO.getShift().name().equalsIgnoreCase("AFTERNOON") &&
+                !inscriptionApplicationDTO.getShift().name().equalsIgnoreCase("NIGHT")){
+            return new ResponseEntity<>("Wrong shift, the shift available are 'MORNING', 'AFTERNOON' and 'NIGHT'", HttpStatus.FORBIDDEN);
+        }
+        // Set changes
+        inscriptionToModify.setUser(userToEnroll); // User
+        inscriptionToModify.setCourse(courseToEnroll); // Course
+        inscriptionToModify.setShift(inscriptionApplicationDTO.getShift()); // Shift
 
+        return new ResponseEntity<>("Inscription modified successfully", HttpStatus.ACCEPTED);
     }
+    @DeleteMapping("/api/inscriptions/{id}")
+    public void deleteInscriptions(@PathVariable Long id){
+        Inscription inscriptionToDelete = inscriptionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Inscription not found for this id ::" + id));
+        // Delete inscription
+        inscriptionRepository.delete(inscriptionToDelete);
+    }
+
 }
